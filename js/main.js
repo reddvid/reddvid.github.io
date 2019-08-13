d962ba44783a14fd7a8ff5a4fb29e64a10fa1ed5
David Ballesteros <david_ballesteros@outlook.com>
2019-08-13 16:00:06 +0800

    updated footer and links

--- js/main.js
+++ js/main.js
@@ -12,17 +12,6 @@ function app_click(appId) {
     window.open('https://www.microsoft.com/store/apps/' + appId, '_blank');
 }
 
-function menuItemClick(station) {
-  var url;
-  if (station == "batac") {
-    url = "http://87.98.130.255:" + 8469;
-  }
-
-  document.getElementById("footer").innerHTML = 
-   "radiostream = \"http:\/\/87.98.130.255:8469\"; artwork = \"http:\/\/html5radios.svnlabs.com\/radio2014\/images\/logo.png\"; width = \"320\";      height = \"200\"; title = \"Test Title\"; artist = \"Test Artist\"; source =      \"shoutcast\"; autoplay = \"true\"; artwork =      \"http:\/\/html5radios.svnlabs.com\/radio2014\/images\/logo.png\";"
-  
-}
-
 function show_html() {
   var z, i, elmnt, file, xhttp;
   /*loop through a collection of all HTML elements:*/

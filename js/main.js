6d50c1199c360337a2c35a41bff247c7d454b868
David Ballesteros <david_ballesteros@outlook.com>
2019-08-13 18:11:44 +0800

    added json logic

--- js/main.js
+++ js/main.js
@@ -5,38 +5,79 @@ function open_menu() {
 /* CLOSE NAV */
 function close_menu() {
     document.getElementById("sidenav").style.width = "0";
-} 
+}
+
+function hide_info() {
+    var element = document.getElementById("app-info");
+    var element2 = document.getElementById("info-area");
+    if (getComputedStyle(element, null).display === "block") {
+        element.style.display = "none";
+        element2.style.display = "none";
+    }
+}
 
 /* APPS CLICK */
 function app_click(appId) {
-    window.open('https://www.microsoft.com/store/apps/' + appId, '_blank');
+    // Load app info
+    var app_name = document.getElementById("app-name");
+    var app_icon = document.getElementById("app-icon");
+    var app_desc = document.getElementById("app-desc");
+
+    console.log(appId);
+    // Get json text
+    var request = new XMLHttpRequest();
+    request.open("GET", "../../assets/apps.json", false);
+    request.send(null)
+    var app_list = JSON.parse(request.responseText);
+
+    for (var i = 0; i < app_list.length; i++) {
+        console.log(i);
+        var item = app_list[i];
+        if (item["appid"].toLower() == appId) {
+
+            console.log(item["name"]);
+            console.log(item["description"]);
+            app_name.innerText = item["name"];
+            app_desc.innerText = item["description"];
+
+            break;
+        }
+    }
+
+    document.getElementById("app-info").style.display = "block";
+    document.getElementById("info-area").style.display = "block";
+
+    // window.open('https://www.microsoft.com/store/apps/' + appId, '_blank');
 }
 
 function show_html() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("w3-include-html");
-      if (file) {
-          /*make an HTTP request using the attribute value as the file name:*/
-          xhttp = new XMLHttpRequest();
-          xhttp.onreadystatechange = function () {
-              if (this.readyState == 4) {
-                  if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-                  if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                  /*remove the attribute, and call this function once more:*/
-                  elmnt.removeAttribute("w3-include-html");
-                  show_html();
-              }
-          }
-          xhttp.open("GET", file, true);
-          xhttp.send();
-          /*exit the function:*/
-          return;
-      }
-  }
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("w3-include-html");
+                    show_html();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            return;
+        }
+    }
 }
-
